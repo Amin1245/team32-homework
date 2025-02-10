e5bd20edae77a01e522c2b84468afb5a1ba6874c
Amin <158226251+Amin1245@users.noreply.github.com>
2025-02-10 20:28:29 +0100

    I divided my homework ,It's the future age

--- javascript/javascript1/week1/Exercise.js
+++ javascript/javascript1/week1/Exercise.js
@@ -1,51 +1,64 @@
 //Age-ify (A future age calculator)
 
 const yearOfBirth = 1988;
-const yearFuture = 2038;
+const yearFuture = 2028;
 const age = yearFuture - yearOfBirth ;
-console.log("You will be " + age +" years old in " + yearFuture );
+console.log(`You will be ${age} years old in ${yearFuture}`);
+
+ 
 
 
 
-
-
-//Goodboy-Oldboy (A dog age calculator)
+ //Goodboy-Oldboy (A dog age calculator)
 
 const dogYearOfBirth =  2019;
-const dogYearFuture =2045;
+const dogYearFuture =2025;
 const dogYear = dogYearFuture - dogYearOfBirth ;
 const shouldShowResultInDogYears = false;
-if (shouldShowResultInDogYears) {
-    console.log("Your dog will be "+  dogYear  + " dog years old in "+ dogYearFuture)
-} else {
-    console.log("Your dog will be "+ dogYear * 7 + " human years old in " + dogYearFuture) 
-};
-
-
-
-
+console.log(
+shouldShowResultInDogYears
+    ?`Your dog will be ${dogYear} dog years old in  {dogYearFuture}`
+     :`Your dog will be ${dogYear * 7} human years old in ${dogYearFuture}`
+);
 //Housey pricey (A house price estimator)
 
 const height = 10;
-const wide = 8;
+const width = 8;
 const deep = 10;
-volumeInMeters = deep * wide * height;
-gardenSizeInM2=100;
-console.log("Peter house Price Is "+(housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300));
+const peterPaid = 2500000;
+const volumePeter = deep * width * height;
+const gardenSizePeter=100;
+const actualPricePeter = volumePeter * 2.5 * 1000 + gardenSizePeter * 300
+console.log(`Peter house Price Is ${actualPricePeter}`);
+    console.log (
+        actualPricePeter < peterPaid
+        ? "This house is a great deal! The owner is paying too little"
+        :"This house is too expensive!"
+    );
 
 const height2 = 8;
-const wide2 = 5;
+const width2 = 5;
 const deep2 = 11;
-volumeInMeters = deep2 * wide2 * height2;
-gardenSizeInM2=100;
-console.log("Julia house Price Is "+(housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300));
+const volumeJulia = deep2 * width2 * height2;
+const gardenSizeJulia=100;
+const juliaPaid = 1000000;
+const actualPriceJulia = volumeJulia * 2.5 * 1000 + gardenSizeJulia * 300
+console.log(`Julia house Price Is ${actualPriceJulia}`);
+console.log (
+    actualPriceJulia < juliaPaid
+    ? "This house is a great deal! The owner is paying too little"
+    :"This house is too expensive!"
+);
 
 
 
+// 
 const firstWords = [ "Easy", "Creative", "Tech", "Smart", "Future", "Digital", "Next", "Global", "Prime", "Quick",];
 const secondWords =["Solutions", "Innovations", "Ventures", "Systems", "Labs", "Startups", "Ideas", "Technologies", "Works", "Studios"];
 const randomNumber1 = Math.floor(Math.random() * 10);
 const randomNumber2 = Math.floor(Math.random() * 10);
 const startupName =firstWords[randomNumber1] + " "+secondWords[randomNumber2];
-const namelength = startupName.length;
-console.log("The startup name Is:" + startupName +" "+ " contains " +namelength + " characters ");
+const nameLength = startupName.length;
+console.log(`The startup name Is: "${startupName}" , contains ${nameLength} characters`);
+
+
