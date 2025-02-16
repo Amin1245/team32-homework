ea321117642e392cb64fe8d4320fcb3624888332
Amin <158226251+Amin1245@users.noreply.github.com>
2025-02-16 23:21:06 +0100

    Update Weather-wear.js
    Thanks for the tips!

--- javascript/javascript1/week2/Weather-wear.js
+++ javascript/javascript1/week2/Weather-wear.js
@@ -1,7 +1,7 @@
 function weather(t) {
     return t > 25 ? "It's beach time...!" 
-         : t > 15 && t <= 25 ? "It's time to wear a sweatshirt!"
-         : t > 5 && t <= 15 ? "You need a warm jacket!"      
+         : t > 15 ? "It's time to wear a sweatshirt!"
+         : t > 5  ? "You need a warm jacket!"      
          : "It's freezing! Stay HOME!";
 }
 
